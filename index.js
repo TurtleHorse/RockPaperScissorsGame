c622715de5cf151b4a4344bb5dd1afa4cc01159b
Chris <christopherjordan30@aol.com>
2022-10-11 17:20:59 -0700

    Added a new game that calculates sleep hours and tells the user if they need to sleep more or less or they sleep the right amount of hours

--- index.js
+++ index.js
@@ -53,3 +53,69 @@ const getUserChoice = (userInput) => {
   }
   
   playGame();
+
+
+
+
+  //CALCULATING SLEEP HOURS
+  // const getSleepHours = day => {
+  //   if(day === 'monday'){
+  //     return 9;
+  //   }
+  //   else if(day === 'tuesday'){
+  //     return 9;
+  //   }
+  //    else if(day === 'wednesday'){
+  //     return 7;
+  //   }
+  //    else if(day === 'thursday'){
+  //     return 6;
+  //   }
+  //    else if(day === 'friday'){
+  //     return 10;
+  //   }
+  //    else if(day === 'saturday'){
+  //     return 8;
+  //   }
+  //    else if(day === 'sunday'){
+  //     return 9;
+  //   }
+  // }
+  
+  // const getActualSleepHours = () => 
+  //   getSleepHours('monday') + 
+  //   getSleepHours('tuesday') +
+  //   getSleepHours('wednesday') +
+  //   getSleepHours('thursday') +
+  //   getSleepHours('friday') +
+  //   getSleepHours('saturday') +
+  //   getSleepHours('sunday');
+    
+  // console.log(getSleepHours('monday'))
+  //   console.log(getActualSleepHours())
+    
+  // const getIdealSleepHours = () => {
+  //   let idealHours = 8;
+  //   return idealHours * 7;
+  // };
+  
+  // const calculateSleepDebt = () => {
+  //   const actualSleepHours = getActualSleepHours();
+  //   const idealSleepHours = getIdealSleepHours();
+  
+  //   if (actualSleepHours === idealSleepHours){
+  //     console.log("User got the perfect amount of sleep")
+  //   }
+  //  else if (actualSleepHours > idealSleepHours){
+  //     console.log("user got " + (actualSleepHours - idealSleepHours) + " more sleep than need")
+  //   }
+  //  else if (actualSleepHours < idealSleepHours){
+  //     console.log('User Should get some rest b/c you slept ' + ( idealSleepHours - actualSleepHours) + " Hours less than you should")
+  //   }
+  
+  //   else {
+  //     console.log('ERROR! Something went wrong, check your code!')
+  //   }
+  // };
+  
+  // calculateSleepDebt();
